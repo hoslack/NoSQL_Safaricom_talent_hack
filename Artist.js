ec1750688f9c0f0d16bc18358e5b64041472597c
hoslack <hoslackochieng@gmail.com>
2018-03-03 13:02:17 +0300

    Revert "Completed the crud operations"
    This reverts commit d6ba6199b92f102528521fed1f647d3e4b4dee64.

--- Artist.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const { Schema } = mongoose;
-
-const artistSchema = new Schema({
-  name: String,
-});
-
-module.exports = mongoose.model('artist', artistSchema);
